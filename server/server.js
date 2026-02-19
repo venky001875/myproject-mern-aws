54b457b832a1516729bc6365abbed5e20d71950f
venky <santhanavenkadeshm@gmail.com>
2026-02-19 14:45:28 +0530

    initial

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Backend Running Successfully");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
